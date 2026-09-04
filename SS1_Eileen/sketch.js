c25e795d1600efcbe1db326277674ec2c7e766d2
CodeQueen1632 <ladyt1632@gmail.com>
2026-09-04 12:55:03 -0400

    added SS_1 study

---
+++ SS1_Eileen/sketch.js
@@ -0,0 +1,16 @@
+/*
+ Name Eileen */
+ function setup() {
+  createCanvas(600, 600);}
+
+function draw() {
+  background("green");
+
+ellipse(200,200,200,200,5);
+push()
+fill(255,0,0);
+
+rect(50,100,50,100);
+pop()
+
+}
